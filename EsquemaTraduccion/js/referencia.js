e769c177ff7e372ae64900ab60e0aa6e783335fd
Alan Diaz <82740575+NinjaDiaz007@users.noreply.github.com>
2023-10-25 15:06:51 -0600

    Add files via upload

---
+++ EsquemaTraduccion/js/referencia.js
@@ -0,0 +1,9 @@
+(function(){
+    //Declaracion de variables
+    let link1 = document.getElementById('link1');
+    let link2 = document.getElementById('link2');
+
+    //Link a las paginas usadas
+    link1.href = "https://enlinea.zacatecas.tecnm.mx/pluginfile.php/10834/mod_resource/content/1/1.5%20Esquemas%20de%20Traduccion.pdf";
+    link2.href = "https://esquema.net/traduccion/";
+})();
